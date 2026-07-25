1e390b98f5c2c48d867abcd6af45170e1d0ed042
Hridya Sharma <hridyasharma2529@gmail.com>
2026-07-26 01:57:23 +0530

    Refactor generate_embeddings command for improved readability and structure

--- hsnManager/management/commands/generate_embeddings.py
+++ hsnManager/management/commands/generate_embeddings.py
@@ -1,20 +1,26 @@
-from hsnManager.models import HSNCode
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+from django.core.management.base import BaseCommand
 from openai import OpenAI
+from hsnManager.models import HSNCode
 
-client = OpenAI()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-rows = HSNCode.objects.filter(embedding__isnull=True)
+class Command(BaseCommand):
+    help = "Generate embeddings for HSN descriptions"
 
-for row in rows:
-    text = row.description
-    response = client.embeddings.create(
-    model="text-embedding-3-small",
-    input=text
-    )
-    embedding = response.data[0].embedding
-    row.embedding = embedding
-    row.save()
+    def handle(self, *args, **options):
+        rows = HSNCode.objects.filter(embedding__isnull=True)
+
+        for row in rows:
+            response = client.embeddings.create(
+                model="text-embedding-3-small",
+                input=row.description,
+            )
+
+            row.embedding = response.data[0].embedding
+            row.save(update_fields=["embedding"])
+
+            self.stdout.write(f"Embedded {row.code}")
